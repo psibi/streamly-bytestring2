d6954f540def5b5b5b81818ae9c99e291352279b
Sibi Prabakaran <sibi@psibi.in>
2018-02-05 00:11:58 +0530

    ADd initial code

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,35 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Main where
 
 import Lib
 import Streamly.ByteString
 import Streamly
 import Data.ByteString
+import Control.Monad.Trans.Resource
 
 -- import Streamly.Prelude (toHandle)
 import qualified System.IO as IO
 
+echo :: IO ()
 echo = toHandle IO.stdout (stdin :: StreamT IO ByteString)
 
+myFile
+  :: (MonadResource m)
+  => StreamT m ByteString
+myFile = fromFile "/home/sibi/xebia.md"
+
+myFile2
+  :: (MonadResource m)
+  => StreamT m ByteString
+myFile2 = fromFile "/home/sibi/tax.md"
+
+bigF
+  :: (MonadResource m)
+  => StreamT m ByteString
+bigF = fromFile "/home/sibi/Downloads/sample.pdf"
+
 main :: IO ()
-main = echo
+main = do
+  xs <- runResourceT $ toFile "/home/sibi/jim.pdf" bigF
+  print xs
